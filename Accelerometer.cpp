88c7f3b6192b04137864666c8ec58dc9f4d51ef5
Andrew Buckley <abuck17@gmail.com>
2021-08-26 18:10:31 -0400

    Initial accelerometer class layout

--- Accelerometer.cpp
+++ Accelerometer.cpp
@@ -1,5 +1,29 @@
 #include "Accelerometer.h"
 
 Accelerometer::Accelerometer() {
+
+  // If the arduino has a accelerometer
+  if (!IMU.accelerationAvailable()) {
+    // TODO: Need to handle this error
+    Serial.print("Error! -> Acceleromter.cpp");
+  }
   
+}
+
+// Return if the read was successful
+bool Accelerometer::updateData() {
+  // Read accelerometer data
+  return IMU.readAcceleration(x, y, z);  
+}
+
+float Accelerometer::getXData() {
+  return x;
+}
+
+float Accelerometer::getYData() {
+  return y;
+}
+
+float Accelerometer::getZData() {
+  return z;
 }
--- Accelerometer.h
+++ Accelerometer.h
@@ -1,10 +1,22 @@
 #ifndef MY_ACCELEROMETER_H
 #define MY_ACCELEROMETER_H
 
+#include <Arduino.h>
+#include <Arduino_LSM9DS1.h>
+
 class Accelerometer {
+
+  private:  
+    float x;
+    float y;
+    float z;
 
   public:
     Accelerometer();
+    bool updateData();
+    float getXData();
+    float getYData();
+    float getZData();
     
 };
 
